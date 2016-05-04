bfd24f101275b841095d90f2eb5f666fc464f6c7
Andres Solorzano <andres_solorzano85@hotmail.com>
2016-05-03 23:39:52 -0500

    Configuring Docker using Ansible for the balancer node

--- hiperium-identity/bootstrap.sh
+++ hiperium-identity/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
 set -e
-
+echo "Installing Ansible..."
+apt-get install -y software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
 echo "Installing Apache Web Server..."
 sudo apt-get install -y apache2
